599106cf6508222c4a3dda62b3de3dcc67e5b1fe
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2019-05-09 22:30:11 -0300

    Adding post
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -1,5 +1,6 @@
 const schedule = require('node-schedule');
 const mongoose = require('mongoose');
+const axios = require('axios');
 const UserSchema = require('../db/userSchema');
 
 const UserModel = mongoose.model('UserModel', UserSchema);
@@ -14,22 +15,22 @@ module.exports = {
       if (time == -1) {
         time = 23;
         date = `${today.getDate() - 1}/${today.getMonth() + 1}/${today.getFullYear()}`;
-        weekDay-=1;
+        weekDay -= 1;
       } else if (time == -2) {
         time = 22;
         date = `${today.getDate() - 1}/${today.getMonth() + 1}/${today.getFullYear()}`;
-        weekDay-=1;
+        weekDay -= 1;
       } else if (time == -3) {
         time = 21;
         date = `${today.getDate() - 1}/${today.getMonth() + 1}/${today.getFullYear()}`;
-        weekDay-=1;
+        weekDay -= 1;
       }
     }
 
     const hours = `${time.toString()}:${today.getMinutes()}:${today.getSeconds()}`;
     const dateTime = `${date.toString()} ${hours.toString()}`;
     // return (dateTime);
-    return(weekDay)
+    return (weekDay)
   },
 
   dailySchedule: () => {
@@ -41,21 +42,21 @@ module.exports = {
   notificationSchedule: () => {
     const weekDay = getDateTime();
     const notificationArray = getDailyNotifications(weekDay);
-    
-    for(let i=0; i<notificationArray.length; i+=1) {
+
+    for (let i = 0; i < notificationArray.length; i += 1) {
       makeSchedule(notificationArray[i]);
     }
   },
-  
+
   getDailyNotifications: (weekDay) => {
     const notificationArray = new Array();
 
     return new Promise(() => {
-      UserModel.find({class: 'user'}).then((userArray) => {
-        for(let i=0; i<userArray.length; i+=1) {
-          for(let j=0; j<userArray[i].notification.length; j+=1) {
-            for(let k=0; k<userArray[i].notification[j].days.length; k+=1) {
-              if(userArray[i].notification[j].days[k] == weekDay) {
+      UserModel.find({ class: 'user' }).then((userArray) => {
+        for (let i = 0; i < userArray.length; i += 1) {
+          for (let j = 0; j < userArray[i].notification.length; j += 1) {
+            for (let k = 0; k < userArray[i].notification[j].days.length; k += 1) {
+              if (userArray[i].notification[j].days[k] == weekDay) {
                 notificationArray.push(userArray[i].notification[j]);
               }
             }
@@ -68,12 +69,19 @@ module.exports = {
 
   makeSchedule: async (notification) => {
 
-    for(let i=0; i<notification.hours.length; i+=1) {
+    for (let i = 0; i < notification.hours.length; i += 1) {
       schedule.scheduleJob(`* * ${notification.hours[i]} * * *`, () => {
-        //post pra gaia
+
         return;
       });
-      
+
     }
-  }
+  },
+  postNotification: (notification) => {
+    return new Promise((resolve) => {
+      axios.post('http://68.183.43.29:30000/notify', notification).then((res) => {
+        resolve(res.body);
+      })
+    })
+  },
 };
