72eb6dbaa60abe78d20e4721bccd8730c545ac17
Eduardo Lima <eduardolima.df@gmail.com>
2019-05-18 01:33:22 -0300

    Adding timeMath tests

--- src/test/notificationTest.js
+++ src/test/notificationTest.js
@@ -17,11 +17,21 @@ describe('Notification test', () => {
     notification.setSport('test');
     notification.setTime(0, 0);
     notification.appendLocal('test');
+    notification.setHoursBefore(0);
+    notification.setMinutesBefore(0);
+    notification.setDay(0);
+    notification.setMonth(0);
+    notification.setYear(0);
 
     notification.getDays(0).should.eql('test');
     notification.getSport().should.eql('test');
     notification.getMinutes().should.eql(0);
     notification.getHours().should.eql(0);
     notification.getLocal(0).should.eql('test');
+    notification.getHoursBefore().should.eql(0);
+    notification.getMinutesBefore().should.eql(0);
+    notification.getDay().should.eql(0);
+    notification.getMonth().should.eql(0);
+    notification.getYear().should.eql(0);
   });
 });
